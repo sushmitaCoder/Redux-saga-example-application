30d5f75a800b580a2a616d73a0952ea6488451c1
Code Sandbox <hello@codesandbox.io>
2021-12-05 13:59:49 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8fdefd6408cf62087e6fe5178b200a7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0f3476662086ee36078d8699112334",
+    "url": "https://sushmitapaulhrc.github.io/csb-rr37e/index.html"
+  },
+  {
+    "revision": "5e6f17855bbf58e9994c",
+    "url": "https://sushmitapaulhrc.github.io/csb-rr37e/static/css/main.a4ea2e08.chunk.css"
+  },
+  {
+    "revision": "4e76ace856ec30d69d72",
+    "url": "https://sushmitapaulhrc.github.io/csb-rr37e/static/js/2.d629ac76.chunk.js"
+  },
+  {
+    "revision": "5e6f17855bbf58e9994c",
+    "url": "https://sushmitapaulhrc.github.io/csb-rr37e/static/js/main.48d38a00.chunk.js"
+  },
+  {
+    "revision": "864e8b98409470471486",
+    "url": "https://sushmitapaulhrc.github.io/csb-rr37e/static/js/runtime~main.f2351b27.js"
+  }
+]);
